9fa5fc3c6eaf043f2a67e049f749f81e28872c46
Devskiller <owl@devskiller.com>
2022-10-01 18:35:49 +0000

    Candidate repo initialization

---
+++ UalaTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UalaTest
+//
+//  Created by Jorge Gastón Mira on 15/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
