52881e59a51ffbfaf346d5d9ae5a721c0771dc6f
Francisco Ferrante <francisco.ferrante@globant.com>
2022-10-01 16:23:12 -0300

    Added the main interactor class

---
+++ UalaTest/Modules/MainModule/Interactor/MainInteractor.swift
@@ -0,0 +1,32 @@
+//
+//  MainInteractor.swift
+//  UalaTest
+//
+//  Created by Francisco Ferrante on 01/10/2022.
+//
+
+import Foundation
+
+// MARK: Main Interactor
+class MainInteractor: MainInteractorProtocol {
+  // MARK: - Main Interactor Variables
+  weak var presenter: MainPresenterProtocol?
+  var apiDataManager: MainDataManagerProtocol?
+  
+  // MARK: - Main Interactor Methods
+  func getData() {
+    apiDataManager?.fetchRecipeData() { [weak self] data in
+      guard let self = self else {
+        return
+      }
+      
+      switch data {
+      case .success(let result):
+        self.presenter?.didFetchRecipeData(with: result)
+      case .failure:
+        self.presenter?.failedToFetchData()
+        break
+      }
+    }
+  }
+}
